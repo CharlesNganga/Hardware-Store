65ee5dd9f443cee664dc2864138da22e3309cc9f
CharlesNganga <ngangacharles44@gmail.com>
2025-09-10 02:24:24 +0300

    Updated Navbar links to follow react router dom

--- h-store/src/components/navigation/Navbar.jsx
+++ h-store/src/components/navigation/Navbar.jsx
@@ -1,5 +1,6 @@
 import { ChevronDown, Heart, Search, ShoppingCart, User } from "lucide-react";
 
+import { Link } from "react-router-dom";
 import Logo from "../../assets/Logo.png";
 import React from "react";
 
@@ -12,47 +13,90 @@ const Navbar = () => {
     <nav className="w-full bg-white shadow-md px-6 py-4 flex items-center justify-between relative">
       {/* Logo */}
       <div className="flex items-center space-x-2">
-        <img src={Logo} alt="Logo" className="h-12 w-auto" />
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="h-12 w-auto" />
+        </Link>
       </div>
 
       {/* Links */}
       <ul className="flex space-x-8 font-medium text-black relative">
-        <li className="hover:text-[#1228e1] cursor-pointer">Home</li>
+        <li>
+          <Link
+            to="/"
+            className="hover:text-[#1228e1] cursor-pointer transition-colors duration-200"
+          >
+            Home
+          </Link>
+        </li>
 
         {/* Products with Dropdown */}
         <li className="relative group cursor-pointer">
-          <div className="flex items-center space-x-1 hover:text-[#1228e1]">
-            <span>Products</span>
+          <div className="flex items-center space-x-1 hover:text-[#1228e1] transition-colors duration-200">
+            <Link to="/products">Products</Link>
             <ChevronDown size={16} />
           </div>
 
           {/* Dropdown Menu */}
           <ul className="absolute left-0 top-full mt-2 w-56 bg-white border border-gray-200 shadow-lg rounded-md opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-200 z-50">
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Plumbing & Piping</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Electrical</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Carpentry</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Steel</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Glues</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Paints</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Gas</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Curtains</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Cement</li>
-            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">Home Equipment</li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/plumbing-piping">Plumbing & Piping</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/electrical">Electrical</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/carpentry">Carpentry</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/steel">Steel</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/glues">Glues</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/paints">Paints</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/gas">Gas</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/curtains">Curtains</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/cement">Cement</Link>
+            </li>
+            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <Link to="/categories/home-equipment">Home Equipment</Link>
+            </li>
           </ul>
         </li>
 
-        <li className="hover:text-[#1228e1] cursor-pointer">Blog</li>
-        <li className="hover:text-[#1228e1] cursor-pointer">Contact Us</li>
+        <li>
+          <Link
+            to="/blog"
+            className="hover:text-[#1228e1] cursor-pointer transition-colors duration-200"
+          >
+            Blog
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/contact"
+            className="hover:text-[#1228e1] cursor-pointer transition-colors duration-200"
+          >
+            Contact Us
+          </Link>
+        </li>
       </ul>
 
       {/* Icons */}
       <div className="flex space-x-6 text-black relative">
-        <Search className="w-5 h-5 cursor-pointer hover:text-[#1228e1]" />
-        <User className="w-5 h-5 cursor-pointer hover:text-[#1228e1]" />
+        <Search className="w-5 h-5 cursor-pointer hover:text-[#1228e1] transition-colors duration-200" />
+        <User className="w-5 h-5 cursor-pointer hover:text-[#1228e1] transition-colors duration-200" />
 
         {/* Favorites with badge */}
         <div className="relative">
-          <Heart className="w-5 h-5 cursor-pointer hover:text-[#1228e1]" />
+          <Heart className="w-5 h-5 cursor-pointer hover:text-[#1228e1] transition-colors duration-200" />
           <span className="absolute -top-2 -right-2 bg-[#1228e1] text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
             {favCount}
           </span>
@@ -60,7 +104,7 @@ const Navbar = () => {
 
         {/* Cart with badge */}
         <div className="relative">
-          <ShoppingCart className="w-5 h-5 cursor-pointer hover:text-[#1228e1]" />
+          <ShoppingCart className="w-5 h-5 cursor-pointer hover:text-[#1228e1] transition-colors duration-200" />
           <span className="absolute -top-2 -right-2 bg-[#1228e1] text-white text-xs font-bold w-5 h-5 flex items-center justify-center rounded-full">
             {cartCount}
           </span>
